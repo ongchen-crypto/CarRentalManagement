174c070cde036ad67003a16792f8373518fceb8d
WOO ONG CHEN 2301208A <2301208A@student.tp.edu.sg>
2024-11-19 01:00:27 +0800

    Added database and seed data

--- CarRentalManagement/Data/CarRentalManagementContext.cs
+++ CarRentalManagement/Data/CarRentalManagementContext.cs
@@ -2,10 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using CarRentalManagement.Data;
 using CarRentalManagement.Configurations.Entities;
-
 namespace CarRentalManagement.Data
 {
-    public class CarRentalManagementContext(DbContextOptions<CarRentalManagementContext> options) : IdentityDbContext<CarRentalManagementUser>(options)
+    public class CarRentalManagementContext(DbContextOptions<CarRentalManagementContext> options) :
+    IdentityDbContext<CarRentalManagementUser>(options)
     {
         public DbSet<CarRentalManagement.Domain.Make> Make { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Model> Model { get; set; } = default!;
@@ -13,7 +13,6 @@ namespace CarRentalManagement.Data
         public DbSet<CarRentalManagement.Domain.Vehicle> Vehicle { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Booking> Booking { get; set; } = default!;
         public DbSet<CarRentalManagement.Domain.Customer> Customer { get; set; } = default!;
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
@@ -21,6 +20,6 @@ namespace CarRentalManagement.Data
             builder.ApplyConfiguration(new MakeSeed());
             builder.ApplyConfiguration(new ModelSeed());
         }
-
     }
 }
+
